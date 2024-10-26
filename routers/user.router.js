912381baf04a04bfc14df05bdb9bab6e33ff47f1
abdullah ahmed <abdallaroom25@gmail.com>
2024-10-27 01:44:16 +0300

    update user routes

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { protectRoute } from "../utils/protectedRoute.js"
-import { getCurrentUser, loginUser, logOut, signUpUser } from "../controllers/user.controller.js"
+import { deleteUser, getCurrentUser, loginUser, logOut, signUpUser, updateUser } from "../controllers/user.controller.js"
 
 
 const router = express.Router()
@@ -9,8 +9,8 @@ const router = express.Router()
 router.get("/me",protectRoute,getCurrentUser)
 router.post("/signup",signUpUser)
 router.post("/login",loginUser)
-router.patch("/update/:userId",logOut)
-router.delete("/delete/:userId",logOut)
+router.patch("/update/:userId",updateUser)
+router.delete("/delete/:userId",deleteUser)
 router.post("/logout",logOut)
 
 export default router
