eb31656c986c5c13777ba54e584b69f658b540cc
abdullah ahmed <abdallaroom25@gmail.com>
2024-10-27 03:28:31 +0300

    origin edit url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import cookieParser from "cookie-parser"
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: true,
+    origin: "http://localhost:5173",
     credentials: true
 }));
 app.use(cookieParser())
