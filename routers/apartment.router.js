39f5d7a75d1613532b56295512b6c4f029a58165
abdullah ahmed <abdallaroom25@gmail.com>
2024-11-10 00:39:48 +0200

    add protect route

--- routers/apartment.router.js
+++ routers/apartment.router.js
@@ -1,13 +1,14 @@
 import express from "express"
 import { addApartment, deleteApartment, getAllApartments, getCurrentApartment, updateApartment } from "../controllers/apartment.controller.js"
+import { protectRoute } from "../utils/protectedRoute.js"
 
 const router = express.Router()
 
 
 router.get("/:compoundId",getAllApartments)
 router.get("/find/:id",getCurrentApartment)
-router.post("/add/:compoundId",addApartment)
-router.patch("/update/:compoundId",updateApartment)
-router.delete("/delete/:apartmentId",deleteApartment)
+router.post("/add/:compoundId",protectRoute,addApartment)
+router.patch("/update/:compoundId",protectRoute,updateApartment)
+router.delete("/delete/:apartmentId",protectRoute,deleteApartment)
 
 export default router 
--- routers/compound.router.js
+++ routers/compound.router.js
@@ -1,12 +1,13 @@
 import express from "express"
 import { addCompound, deleteCompound, getAllCompounds, getCurrentCompound, updateCompound } from "../controllers/compound.controller.js"
+import { protectRoute } from "../utils/protectedRoute.js"
 
 const router = express.Router()
 // compound/
-router.get("/",getAllCompounds)
+router.get("/",protectRoute,getAllCompounds)
 router.get("/find/:id",getCurrentCompound)
-router.post("/add",addCompound)
-router.patch("/update/:id",updateCompound)
-router.delete("/delete/:id",deleteCompound)
+router.post("/add",protectRoute,addCompound)
+router.patch("/update/:id",protectRoute,updateCompound)
+router.delete("/delete/:id",protectRoute,deleteCompound)
 
 export default router
